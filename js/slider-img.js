e819bc9190b3a9fa338d52766bf48f123079e3a2
Shambles <elzofovreo@gmail.com>
2023-04-19 14:58:03 -0300

    slider

---
+++ js/slider-img.js
@@ -0,0 +1,76 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    let imagenes = [
+                 {img: './imgs/galeria/1.jpg'},
+                 {img: './imgs/galeria/2.jpg'},
+                 {img: './imgs/galeria/3.jpg'},
+                 {img: './imgs/galeria/4.jpg'},
+                 {img: './imgs/galeria/5.jpg'},
+                 {img: './imgs/galeria/6.jpg'},
+                 {img: './imgs/galeria/7.jpg'},
+                 {img: './imgs/galeria/8.jpg'},
+                 {img: './imgs/galeria/9.jpg'},
+                 {img: './imgs/galeria/10.jpg'},
+                //  {img: './img/galeria/11.jpg'},
+                //  {img: './img/galeria/12.jpg'},
+                //  {img: './img/galeria/13.jpg'},
+                //  {img: './img/galeria/14.jpg'},
+                //  {img: './img/galeria/15.jpg'},
+                //  {img: './img/galeria/16.jpg'},
+                //  {img: './img/galeria/17.jpg'},
+                //  {img: './img/galeria/18.jpg'},
+                //  {img: './img/galeria/19.jpg'},
+                //  {img: './img/galeria/20.jpg'},
+                //  {img: './img/galeria/21.jpg'},
+                //  {img: './img/galeria/22.jpg'},
+                //  {img: './img/galeria/23.jpg'},
+                //  {img: './img/galeria/24.jpg'},
+                //  {img: './img/galeria/25.jpg'}
+             ];
+        
+
+let contador = 0
+const contenedor = document.querySelector('.slideshow');
+const overlay = document.querySelector('.overlay');
+const galeria_imagenes = document.querySelectorAll('.galeria img');
+const img_slideshows = document.querySelector('.slideshow img');
+                 
+contenedor.addEventListener('click', function(event) {
+    let atras = contenedor.querySelector('.atras'),
+        adelante = contenedor.querySelector('.adelante'),
+        img = contenedor.querySelector('img'),
+            tgt = event.target
+            if (tgt == atras) {
+                if (contador > 0) {
+                    img.src = imagenes[contador - 1].img
+                    contador--
+                } else {
+                    img.src = imagenes[imagenes.length - 1].img
+                    contador = imagenes.length - 1
+                }
+            } else if (tgt == adelante) {
+                if (contador < imagenes.length - 1) {
+                    img.src = imagenes[contador + 1].img
+                    contador++
+                } else {
+                    img.src = imagenes[0].img
+                    contador = 0
+                }
+            }
+         
+})
+Array.from(galeria_imagenes).forEach(img => {
+    img.addEventListener('click', event => {
+        const imagen_seleccionada = +(event.target.dataset.imgMostrar)
+            img_slideshows.src = imagenes[imagen_seleccionada].img
+            contador = imagen_seleccionada
+            overlay.style.opacity = 1
+            overlay.style.visibility = 'visible'
+    })
+})
+         
+var span = document.getElementsByClassName("btn_cerrar")[0];
+    span.onclick = function() {
+        overlay.style.visibility = "hidden";
+    }
+})
